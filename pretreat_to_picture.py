e56eee86c60f743a6ba0a485dd001effab630f62
Rflecter510 <499300020@qq.com>
2021-04-06 20:24:23 +0800

    图演示

--- pretreat_to_picture.py
+++ pretreat_to_picture.py
@@ -1,7 +1,6 @@
 import wave
 import numpy as np
 import sklearn
-import zcr
 import librosa
 from python_speech_features import mfcc,logfbank,get_filterbanks,delta
 from matplotlib import pyplot as plt
@@ -28,7 +27,7 @@ class PreProcess():
             librosa.display.waveplot(self.y, sr=self.sr, alpha=0.4)
             #plt.plot(self.y)
             plt.title('After Sampling')
-            plt.show()
+            #plt.show()
         return self.y,self.sr
 
     # 预增强
@@ -39,7 +38,7 @@ class PreProcess():
             librosa.display.waveplot(self.y, sr=self.sr, alpha=0.4)
             #plt.plot(Enhanced_y)
             plt.title('After Pre_Emphasis')
-            plt.show()
+            #plt.show()
         self.y=Enhanced_y
         return self.y
 
@@ -91,7 +90,7 @@ class PreProcess():
             plt.plot(self.y)
 
             plt.title('After Enframe')
-            plt.show()
+            #plt.show()
         return self.y  #返回帧信号矩阵
 
     # 汉明窗 对每帧信号加窗
@@ -104,7 +103,7 @@ class PreProcess():
             #librosa.display.waveplot(self.y, sr=self.sr, alpha=0.4)
             plt.plot(self.y)
             plt.title('After Hamming')
-            plt.show()
+            #plt.show()
         return  self.y
 
     # 提取MFCC和滤波器组特征
@@ -164,7 +163,7 @@ class PreProcess():
         if ifPlot==True:
             plt.plot(zeroCrossingRate)
             plt.title('st-Zero Cross Rate')
-            plt.show()
+            #plt.show()
         return zeroCrossingRate
     # Normalising the spectral centroid for visualisation
 
@@ -177,10 +176,11 @@ def Xdb (y1,gen_data=True):
     Xdb = librosa.amplitude_to_db(abs(X),ref=np.max)            # 获得语谱图
     if gen_data==False:
         plt.figure(figsize=(14, 5))
+    plt.title('Specgram')
     librosa.display.specshow(Xdb, sr=y1.sr, x_axis='time', y_axis='hz')   # 绘制语谱图
     if gen_data==False:
         plt.colorbar()
-        plt.show()
+        #plt.show()
 
 def Spectral_Centroids(y1):
     '''频谱质心'''
@@ -191,7 +191,7 @@ def Spectral_Centroids(y1):
     # Plotting the Spectral Centroid along the waveform
     librosa.display.waveplot(y1.y, sr=y1.sr, alpha=0.4)
     plt.plot(t, normalize(spectral_centroids), color='r')
-    plt.show()
+    #plt.show()
 
 def MFCC(y1,save_dir=None,ifPlot=False):
     '''MFCC '''
@@ -199,7 +199,8 @@ def MFCC(y1,save_dir=None,ifPlot=False):
     # Displaying  the MFCCs:
     librosa.display.specshow(mfccs, sr=y1.sr, x_axis='time')
     if ifPlot == True:
-        plt.show()
+        #plt.show()
+        pass
     if save_dir!=None:
         plt.savefig(save_dir)
 
@@ -208,48 +209,135 @@ def Spectral_Rolloff(y1):
     spectral_centroids = librosa.feature.spectral_centroid(y1.y, sr=y1.sr)[0]
     # Computing the time variable for visualization
     frames = range(len(spectral_centroids))
+    plt.title('Spectral_Rolloff')
     t = librosa.frames_to_time(frames)
     spectral_rolloff = librosa.feature.spectral_rolloff(y1.y + 0.01, sr=y1.sr)[0]
     librosa.display.waveplot(y1.y, sr=y1.sr, alpha=0.4)
     plt.plot(t, normalize(spectral_rolloff), color='r')
-    plt.show()
 
+
+# 自定义函数，计算数值的符号。
+def sgn(data):
+    if data >= 0 :
+        return 1
+    else :
+        return 0
+#计算过零率
+def calZeroCrossingRate(wave_data) :
+    zeroCrossingRate = []
+    sum = 0
+
+    for i in range(len(wave_data)) :
+        #
+        if i % 256 == 0:
+            #print(i)
+            continue
+        sum = sum + np.abs(sgn(wave_data[i]) - sgn(wave_data[i - 1]))
+        if (i + 1) % 256 == 0 :
+            zeroCrossingRate.append(float(sum) / 255)
+            sum = 0
+        elif i == len(wave_data) - 1 :
+            zeroCrossingRate.append(float(sum) / 255)
+    print("done")
+    plt.plot(zeroCrossingRate)
+    plt.title('st-Zero Cross Rate')
+    #plt.show()
+    return zeroCrossingRate
+
+# 计算每一帧的能量 256个采样点为一帧
+def calEnergy(wave_data) :
+    energy = []
+    sum = 0
+    for i in range(len(wave_data)) :
+        sum = sum + (int(wave_data[i]) * int(wave_data[i]))
+        if (i + 1) % 256 == 0 :
+            energy.append(sum)
+            sum = 0
+        elif i == len(wave_data) - 1 :
+            energy.append(sum)
+    plt.plot(energy)
+    plt.title('Short Energy')
+    #plt.show()
+    return energy
+
+def load_wave_data(file_path):
+    f = wave.open(file_path, "rb")
+    # getparams() 一次性返回所有的WAV文件的格式信息
+    params = f.getparams()
+    # nframes 采样点数目
+    nchannels, sampwidth, framerate, nframes = params[:4]
+    # readframes() 按照采样点读取数据
+    str_data = f.readframes(nframes)  # str_data 是二进制字符串
+
+    # 以上可以直接写成 str_data = f.readframes(f.getnframes())
+
+    # 转成二字节数组形式（每个采样点占两个字节）
+    wave_data = np.fromstring(str_data, dtype=np.short)
+    print("采样点数目：" + str(len(wave_data)))  # 输出应为采样点数目
+    f.close()
+    return  wave_data
 
 if __name__=="__main__":
-    file_path='data_audio/is/堂春-105.wav'
-    #file_path = '测试.wav'
-    y1=PreProcess(file_path)
+    file_path='梅兰芳霸王别姬上~1.wav'
+    ifPlot = True  # 是否绘制图像
 
-    ifPlot=True                                          #是否绘制图像
+    y1=PreProcess(file_path)
 #########################################预处理
-    y1.y,y1.sr=y1.sampling(file_path,ifPlot=False,sr=None) #采样
-    '''y1.pre_emphasis( ifPlot=False )                        #预增强
-    y1.enframe(nw=256,inc=128, ifPlot=False )              #分帧
-    y1.force_window( ifPlot=False )                        #加窗
+    y1.y,y1.sr=y1.sampling(file_path,ifPlot=True,sr=None) #采样
+    plt.savefig("采样.png")
+    plt.show()
+
+    y1.pre_emphasis( ifPlot=True )                        #预增强
+    plt.savefig("预增强.png")
+    plt.show()
+
+    y1.enframe(nw=256,inc=128, ifPlot=True )              #分帧
+    plt.savefig("分帧.png")
+    plt.show()
+
+    y1.force_window( ifPlot=True )                        #加窗
+    plt.savefig("加窗.png")
+    plt.show()
 
 #########################################特征提取
-    短时平均能量和短时平均过零率的特征提取在zcr中  
-    wave_data= zcr.load_wave_data(file_path)
-    zcr.calEnergy(wave_data)                                #短时平均能量
-    zeroCrossingRate = zcr.calZeroCrossingRate(wave_data)   #短时平均过零率
-    '''
+
+    #短时平均能量和短时平均过零率的特征提取在zcr中
+    wave_data= load_wave_data(file_path)
+    calEnergy(wave_data)                                #短时平均能量
+    plt.savefig("短时平均能量.png")
+    plt.show()
+
+    zeroCrossingRate = calZeroCrossingRate(wave_data)   #短时平均过零率
+    plt.savefig("短时平均过零率.png")
+    plt.show()
+
     y1.sampling(y1.file_path)
     Xdb(y1)                                                 #频谱图
+    plt.savefig("频谱图.png")
+    plt.show()
+
     Spectral_Centroids(y1)                                  #频谱质心
+    plt.savefig("频谱质心.png")
+    plt.show()
 
     #设置图像显示格式
-    plt.subplot(1, 1, 1)
-    plt.axis('off')
-    fig = plt.gcf()
-    fig.set_size_inches(2.56,2.56)  # 输出width*height像素
-    plt.gca().xaxis.set_major_locator(plt.NullLocator())
-    plt.gca().yaxis.set_major_locator(plt.NullLocator())
-    plt.subplots_adjust(top=1, bottom=0, left=0, right=1, hspace=0, wspace=0)
-    plt.margins(0, 0)
+    # plt.subplot(1, 1, 1)
+    plt.title('MFCC')
+    #plt.axis('off')
+    # fig = plt.gcf()
+    # fig.set_size_inches(2.56,2.56)  # 输出width*height像素
+    #plt.gca().xaxis.set_major_locator(plt.NullLocator())
+    #plt.gca().yaxis.set_major_locator(plt.NullLocator())
+    # plt.subplots_adjust(top=1, bottom=0, left=0, right=1, hspace=0, wspace=0)
+    # plt.margins(0, 0)
 
     y1.sampling(y1.file_path)
     MFCC(y1,ifPlot=True)                                    #MFCC
+    plt.savefig("MFCC.png")
+    plt.show()
 
-    #Spectral_Rolloff(y1)                                   #光谱衰减
+    Spectral_Rolloff(y1)                                   #光谱衰减
+    plt.savefig("光谱衰减.png")
+    plt.show()
 
 
